61b5123a0352b84614fa3f49cd608ad56d0525a5
prajeesh.webandcrafts@gmail.com <prajeesh.webandcrafts@gmail.com>
2024-05-22 17:03:04 +0530

    megamenu

--- src/api/staticData/headerNavLinks.js
+++ src/api/staticData/headerNavLinks.js
@@ -17,7 +17,7 @@ const headerNavLinks = {
           title: "Corporates & Institutes",
           content:
             "We understand the unique demands of the professional, institutional and educational realms. CAFS finds delight in curating safe and healthy culinary experiences that not only meet but also exceed expectations.",
-            imgURL: "/images/serveImg.jpg",
+            imgURL: "/images/serveimgtwo.jpg",
 
         },
 
@@ -26,7 +26,7 @@ const headerNavLinks = {
           title: "Retail Store Catering",
           content:
           "Our retail catering services offer a delightful blend of health and taste for your shelves. At CAFS, we understand the significance of offering your customers nourishing and wholesome options. We are setting a new standard for quality in retail catering.",
-            imgURL: "/images/serveImg3.jpg",
+            imgURL: "/images/serveimgthree.jpg",
 
         },
 
